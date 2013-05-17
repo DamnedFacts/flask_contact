bd9704ac77cbb8b808fcc5f6e7122f8d94bb5c16
BCC Administrator <bcc@pas.rochester.edu>
2013-05-17 13:31:36 -0400

    Added multiple recipient selection

---
+++ config_sample.py
@@ -0,0 +1,8 @@
+MAIL_HOST="localhost"
+MAIL_PORT=25
+MAIL_RECIPIENT={"webmaster": ("Webmaster", "webmaster@host.tld"), 
+        "frank": ("Frank W", "frankw@host.tld"),
+        "admin": ("System Administrator", "admin@host.tld"),
+        }
+MAIL_SUBJECT_PREPEND="[Flask Contact Form] "
+
