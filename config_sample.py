bfc72ea6a8c0d829b41dd3acbf7b6628d5f7d1d9
BCC Administrator <bcc@pas.rochester.edu>
2013-05-17 13:31:36 -0400

    Added multiple recipient selection

---
+++ config_sample.py
@@ -0,0 +1,8 @@
+MAIL_HOST="mail.pas.rochester.edu"
+MAIL_PORT=25
+MAIL_RECIPIENT={"webmaster": ("Webmaster", "bcc@pas.rochester.edu"), 
+        "wolfs": ("Frank Wolfs", "wolfs@pas.rochester.edu"),
+        "bcc": ("Barnes Computing Center", "bcc@pas.rochester.edu"),
+        }
+MAIL_SUBJECT_PREPEND="[Flask Contact Form] "
+
--- contact.py
+++ contact.py
@@ -1,6 +1,36 @@
+"""
+File: contact.py
+Project: flask-contact (https://github.com/DamnedFacts/flask-contact)
+
+Copyright (c) 2013, Richard Emile Sarkis <rich@sarkis.info>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the <organization> nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
 # Contact page
-from flask import (Blueprint, render_template, abort, request, flash)
-from wtforms import Form, TextField, TextAreaField, BooleanField
+from flask import (Blueprint, render_template, request)
+from wtforms import Form, TextField, TextAreaField, BooleanField, SelectField
 from formencode.variabledecode import variable_encode
 import smtplib
 from email.MIMEText import MIMEText
@@ -8,15 +38,22 @@ from email.MIMEText import MIMEText
 # Local Project
 import validation
 
+# import our configuration preferences for the contact form.
+from config import *
+
 contact_page = Blueprint('contact_page', __name__, template_folder="templates",
                          static_folder='static')
 
-MAIL_HOST="localhost"
-MAIL_PORT=25
-MAIL_RECIPIENT="info@myhost.com"
-MAIL_SUBJECT_PREPEND="[Flask Contact Form] "
-
+"""
+The WTForms contact form. Of note:
+    * The "recipient" field which may be overridden in the class before 
+      instantiation (and later restored).
+    * The "antispam" field, which places an invisible checkbox in the form that
+      form bots will see, but real users will not. This will prevent automated
+      spam.
+"""
 class ContactForm(Form):
+    recipient = SelectField("Send To: ", choices=[(k,v[0]) for k,v in MAIL_RECIPIENT.iteritems()])
     name = TextField("Name: ")
     email = TextField("Email: ")
     phone = TextField("Phone: ")
@@ -24,7 +61,12 @@ class ContactForm(Form):
     message = TextAreaField("Message: ")
     antispam = BooleanField("I certify that this is not SPAM.")
 
+"""
+Sends mail upon posting the form.
+"""
 def send_email(dataDict, responseHeaders):
+    recipient = "{0} <{1}>".format(MAIL_RECIPIENT[dataDict["recipient"]][0], MAIL_RECIPIENT[dataDict["recipient"]][1])
+
     # Append dataDict key/value pairs.
     bodyStr = ""
     bodyStr += "Name: %s\n" % dataDict["name"]
@@ -41,12 +83,12 @@ def send_email(dataDict, responseHeaders):
     email = MIMEText(bodyStr)
     email['Subject'] = MAIL_SUBJECT_PREPEND + dataDict["subject"]
     email['From'] = dataDict["email"]
-    email['To'] = MAIL_RECIPIENT
+    email['To'] = recipient
 
     message = email.as_string()
 
     server = smtplib.SMTP(MAIL_HOST,MAIL_PORT)
-    server.sendmail(dataDict["email"], MAIL_RECIPIENT, message)
+    server.sendmail(dataDict["email"], recipient, message)
     server.close()
 
     try:
@@ -55,10 +97,21 @@ def send_email(dataDict, responseHeaders):
         response = "error"
     return response
 
-
+"""
+Flask route for producing the contact form view.
+"""
 @contact_page.route('/', methods=['GET', 'POST'])
 def contact():
+    info = None
+    saved_recipient_field = ContactForm.recipient
+    recip_id = request.args.get('recipient')
+    if recip_id in MAIL_RECIPIENT:
+        recip_name = MAIL_RECIPIENT[request.args.get('recipient')][0]
+        ContactForm.recipient = TextField("Send To: ", default=recip_name,
+                                          id=recip_id)
     form = ContactForm(request.form)
+    ContactForm.recipient = saved_recipient_field
+
     if request.method == "POST":
         # There are three differente responses: thanks, invalid, and error
         response = "thanks"
@@ -68,9 +121,9 @@ def contact():
         responseHeaders = request.headers
 
         # We use this checkbox state as an inverse state of spam
-        # If this is checked, we assume a bot ignored the markup and 
+        # If this is checked, we assume a bot ignored the markup and
         # clicked it.
-        # We then ignore the message, but present a false sense of 
+        # We then ignore the message, but present a false sense of
         # success.
         antispam = True if dataDict.has_key("antispam") else False
 
@@ -102,6 +155,6 @@ def contact():
         if response == 'thanks' :
             form = ContactForm()
 
-        return render_template("contact_t.html", form=form, info=info, page_title="Contact Us")
-    else:
-        return render_template("contact_t.html", form=form, page_title="Contact Us")
+    return render_template("contact_t.html", form=form, info=info,
+                           one_recipient=(recip_id in MAIL_RECIPIENT),
+                           page_title="Contact Us")
